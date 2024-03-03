6f9f87fb5015e112b77c2de8109fa1e5c074a2a2
khadiza21 <bibikhadiza474@gmail.com>
2024-03-03 21:15:43 +0600

    GET and POST methods

--- lab1_assignment.java
+++ lab1_assignment.java
@@ -1,26 +1,23 @@
-package com.mycompany.report1_1;
+package com.mycompany.report_1_2;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Report1_1 {
-
-    private static URL postUrl;
-
+public class Report_1_2 {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://jsonplaceholder.typicode.com/posts/1");       
-      // String postData = "{\"name\":\"Khadiza\",\"job\":\"Software Engineer\"}";
-      // postUrl = new URL("https://reqres.in/api/users");    
-       String postData = "{\"title\":\"Khadiza\",\"completed\":false,\"userId\":1}";
-       postUrl = new URL("https://jsonplaceholder.typicode.com/todos");
-       
-       getRequestMaking(url);
-       postRequestMaking(postUrl, postData);
+        URL url = new URL("https://webcode.com/");
+        getRequestMaking(url);
     }
 
+    // https://.me/
+    // https://github.com/
+    // https://stackoverflow.com/
+    //
     private static void getRequestMaking(URL url) throws IOException {
         HttpURLConnection connect = (HttpURLConnection) url.openConnection();
         connect.setRequestMethod("GET");
@@ -29,52 +26,26 @@ public class Report1_1 {
         int response = connect.getResponseCode();
         System.out.println("Response Code " + response);
         System.out.println("Response message " + connect.getResponseMessage());
+        System.out.println();
 
         if (response == HttpURLConnection.HTTP_OK) {
             BufferedReader read = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-            StringBuffer str = new StringBuffer();
-            String store = null;
-            while ((store = read.readLine()) != null) {
-                str.append(store);
+            StringBuilder str = new StringBuilder();
+            String line;
+            while ((line = read.readLine()) != null) {
+                str.append(line);
             }
+
             read.close();
-            System.out.println("response " + str.toString());
-            System.out.println();
-        }
-    }
 
-    private static void postRequestMaking(URL url, String postData) throws IOException {
-        HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+            Pattern pattern = Pattern.compile("<p>(.*?)</p>");
+            Matcher matcher = pattern.matcher(str.toString());
 
-        connect.setRequestMethod("POST");
-        connect.setRequestProperty("User-Agent", "Chrome");
-        connect.setDoOutput(true);
-
-        try (OutputStream outputStream = connect.getOutputStream()) {
-            byte[] input = postData.getBytes("utf-8");
-            outputStream.write(input, 0, input.length);
-        }
-
-        int response = connect.getResponseCode();
-        System.out.println("Response Code: " + response);
-
-        System.out.println("Response Message: " + connect.getResponseMessage());
-
-        if (response == HttpURLConnection.HTTP_OK || response == HttpURLConnection.HTTP_CREATED) {
-            BufferedReader read = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-            StringBuffer str = new StringBuffer();
-            String store;
-
-            while ((store = read.readLine()) != null) {
-                str.append(store);
+            while (matcher.find()) {
+                System.out.println(matcher.group(1));
             }
-            read.close();
-            System.out.println("post resonse code");
-            System.out.println("Response: " + str.toString());
-            System.out.println("POST request is successful.");
+
             System.out.println();
-        }else{
-            System.out.println("POST request failed. Check the response code and message for details. ");
         }
     }
 }
