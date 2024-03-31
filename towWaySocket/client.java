5a9e3ed14e83a59a91b72ce57ade038e132b2504
khadiza21 <bibikhadiza474@gmail.com>
2024-03-31 12:58:15 +0600

    two way socket communicaion

--- towWaySocket/client.java
+++ towWaySocket/client.java
@@ -4,33 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
-public class server {
+public class client {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(1234); 
-            System.out.println("Server started. Waiting for a client...");
+            Socket socket = new Socket("localhost", 1234);
+            System.out.println("Connected to server.");
 
-            Socket clientSocket = serverSocket.accept(); 
-            System.out.println("Client connected.");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
             BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
 
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("Client: " + inputLine);
-                System.out.print("Server: ");
-                String response = consoleInput.readLine();
-                out.println(response);
+            String userInput;
+            while ((userInput = consoleInput.readLine()) != null) {
+                out.println(userInput);
+                String serverResponse = in.readLine();
+                System.out.println("Server: " + serverResponse);
             }
-
-            serverSocket.close();
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
